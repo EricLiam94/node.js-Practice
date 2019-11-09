c0b02422d5a5058fd99dd870d78d73520d176efc
EricLiam94 <ericliamhjx@gmail.com>
2019-11-09 19:11:40 +1100

    audio

--- router/myAccount.js
+++ router/myAccount.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const POST = require("../models/Post")
-
 const bcryt = require("bcryptjs")
 
 router.post("/", async (req,res)=>{
-    console.log(req.body)
     const result =  await POST.findOne({
         email:req.body.email,
     })
@@ -12,7 +10,10 @@ router.post("/", async (req,res)=>{
     if (!result) return res.render("error" , {Error:"Email does not match any account"})
     var valid = await bcryt.compare(req.body.password,result.password)
     if(!valid) return res.render("error" , {Error:"Invalid Username or Password"})
-    res.render('myAccount',{account:result.email})
+    res.render('myAccount',{
+        account:result.email,
+        audioSrc: "/AudioSrc/summer.mp3"
+    })
 } )
 
 router.get("/",async (req,res)=>{
