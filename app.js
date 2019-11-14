dc99e69c66f832a1a57cd6e3543a4bb43d357503
EricLiam94 <ericliamhjx@gmail.com>
2019-11-14 18:07:35 +1100

    id

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 const signUpRouter = require('./router/signup')
 const myAccountRouter = require('./router/myAccount')
 const POST = require("./models/Post").user
-const notePost =require("./models/Post").note
+const notePost = require("./models/Post").note
 const flash = require('connect-flash')
 const session = require("express-session")
 var sessionVarify = require("./models/verification")
@@ -22,33 +22,35 @@ app.use(session({
     secret: 'keyboard cat',
     resave: true,
     saveUninitialized: true,
-  }))
-app.use(express.static(__dirname + "/public" ))  //Specify where the static file is
-app.use(bodyParser.urlencoded({ extended: true })) 
+}))
+app.use(express.static(__dirname + "/public")) //Specify where the static file is
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.json())
-app.use("/signUp",signUpRouter)
-app.use("/myAccount",myAccountRouter)
+app.use("/signUp", signUpRouter)
+app.use("/myAccount", myAccountRouter)
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.suc_msg = req.flash('suc_msg')
     res.locals.err_msg = req.flash('err_msg')
     res.locals.email = req.flash('email')
     next()
 })
 
-app.use('/myNote',noteRouter)
+app.use('/myNote', noteRouter)
 
 // connect to mongodb
-mongodb.connect(process.env.DB_CONNECTION,
-    { useNewUrlParser: true ,
-        useUnifiedTopology: true}
-    ,()=>{
+mongodb.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
     console.log("mongodb is connected")
 })
 
 //home page
-app.get("/",(req,res)=>{
-    if (req.session &&  req.session.token) return res.redirect('/myAccount')
+app.get("/", (req, res) => {
+    if (req.session && req.session.token) return res.redirect('/myAccount')
     res.render("index")
 })
 
@@ -72,22 +74,22 @@ app.get("/",(req,res)=>{
 //     .then(
 //         console.log("note saved")
 //     )
-    
+
 //     res.redirect("/myNote")
 
 // })
 
-app.get("/about",sessionVarify,(req,res)=>{
-    res.render("about",{
-        account:"user"
+app.get("/about", sessionVarify, (req, res) => {
+    res.render("about", {
+        account: "user"
     })
 })
 
-app.post("/",async (req,res)=>{
-   
+app.post("/", async (req, res) => {
+
 })
 
 //port
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("port 3000 is connected")
 })
--- router/myAccount.js
+++ router/myAccount.js
@@ -4,48 +4,51 @@ const bcryt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const flash = require('connect-flash')
 
-router.post("/", async (req,res)=>{
-    const result =  await POST.findOne({
-        email:req.body.email,
+router.post("/", async (req, res) => {
+    const result = await POST.findOne({
+        email: req.body.email,
     })
 
     if (!result) {
-        req.flash('err_msg','Email does not match any account' )
-    return res.redirect(".")}
-    var valid = await bcryt.compare(req.body.password,result.password)
-    if(!valid){
-        req.flash('err_msg','Invalid Username or Password' )
-        return res.redirect(".")}
-    
+        req.flash('err_msg', 'Email does not match any account')
+        return res.redirect(".")
+    }
+    var valid = await bcryt.compare(req.body.password, result.password)
+    if (!valid) {
+        req.flash('err_msg', 'Invalid Username or Password')
+        return res.redirect(".")
+    }
 
-    var token =  jwt.sign({_id:result._id},process.env.TOKEN_SECRET);
+
+    var token = jwt.sign({
+        _id: result._id
+    }, process.env.TOKEN_SECRET);
     req.session.token = token
     req.session.email = result.email
-    res.render('myAccount',{
-        account:result.email,
+    res.render('myAccount', {
+        account: result.email,
         audioSrc: "/AudioSrc/summer.mp3"
     })
-} )
-
-router.get("/",async (req,res)=>{
-    if(req.session && req.session.token)
-    {
-       jwt.verify(req.session.token,process.env.TOKEN_SECRET,(err,decoded)=>{
-        if(err) return res.redirect('/')
-        else return res.render('myAccount',{
-            account:req.session.email,
-            audioSrc: "/AudioSrc/summer.mp3"
-        })
-       })
-    }
-    else{
-       res.redirect('/')}
 })
 
-router.get("/signOff",(req,res)=>{
-    if(req.session){
-        req.session.destroy((err)=>{
-            if(err) console.log(err)
+router.get("/", async (req, res) => {
+    if (req.session && req.session.token) {
+        jwt.verify(req.session.token, process.env.TOKEN_SECRET, (err, decoded) => {
+            if (err) return res.redirect('/')
+            else return res.render('myAccount', {
+                account: req.session.email,
+                audioSrc: "/AudioSrc/summer.mp3"
+            })
+        })
+    } else {
+        res.redirect('/')
+    }
+})
+
+router.get("/signOff", (req, res) => {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) console.log(err)
             return res.redirect('/')
         })
     }
